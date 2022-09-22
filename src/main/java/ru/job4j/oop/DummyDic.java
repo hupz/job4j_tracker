33cb81aa834f9286b28b0a02f2752c978f518502
Vedmak <icefortess@gmail.com>
2022-09-22 23:21:02 +0300

    1.3. Вызов метода с аргументами. [#166086 #345425]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,7 +2,11 @@ package ru.job4j.oop;
 
 public class DummyDic {
 
-    public String engToRus(String eng) {
+    public static String engToRus(String eng) {
         return "Неизвестное слово." + eng;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(engToRus("Песня"));
     }
 }
