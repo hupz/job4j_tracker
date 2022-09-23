a8b862553ede505f58f129100f03f096e993447b
Vedmak <icefortess@gmail.com>
2022-09-23 23:51:07 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public void printInfo() {
+        System.out.println("Program work? true or false: " + active);
+        System.out.println("How much error in program? - " + status);
+        System.out.println("Message for developer: " + message);
+    }
+
+    public Error() {
+        active = true;
+        status = 0;
+        message = "You program works well";
+    }
+
+    public Error(boolean active, int status, String message ) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(false, 1, "You program don't work");
+        error1.printInfo();
+        Error error2 = new Error(true, 1, "The program is running with an error");
+        error2.printInfo();
+    }
+}
