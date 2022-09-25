977c8eca16d78fde100f781c580c4b349b1a148f
Vedmak <icefortess@gmail.com>
2022-09-25 23:49:37 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        if(this.load == 100) {
+           this.load = 0;
+       }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
