9a7f7fe5bbec7a0a7cf9c88dfbfc05b93a03e61c
Vedmak <icefortess@gmail.com>
2022-11-08 23:35:57 +0300

    3. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -2,6 +2,8 @@ package ru.job4j.poly;
 
 public interface Transport {
     public void ride();
+
     public void passengers(int count);
-    public int refuel (int fuel);
+
+    public int refuel(int fuel);
 }
