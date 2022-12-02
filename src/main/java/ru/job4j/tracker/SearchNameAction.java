003f7aec3283f651c14e7fd14b0dfda48a549d38
Vedmak <icefortess@gmail.com>
2022-12-03 00:09:00 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/SearchNameAction.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+public class SearchNameAction implements UserAction {
+    @Override
+    public String name() {
+        return "Find item for Name";
+    }
+
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Find items by name ===");
+        String name = input.askStr("Enter name: ");
+        Item[] items = tracker.findByName(name);
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("Заявки с именем: " + name + " не найдены.");
+        }
+        return true;
+    }
+}
