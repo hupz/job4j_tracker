740186351bfff85c589b1380a4d290dd3fa04239
Vedmak <icefortess@gmail.com>
2022-12-05 23:40:32 +0300

    9.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/EditAction.java
+++ src/main/java/ru/job4j/tracker/EditAction.java
@@ -1,20 +1,26 @@
 package ru.job4j.tracker;
 
 public class EditAction implements UserAction {
+    private final Output out;
+
+    public EditAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Edit item";
     }
 
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Edit item ===");
+        out.println("=== Edit item ===");
         int id = input.askInt("Enter id: ");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
-            System.out.println("Заявка изменена успешно.");
+            out.println("Заявка изменена успешно.");
         } else {
-            System.out.println("Ошибка замены заявки.");
+            out.println("Ошибка замены заявки.");
         }
         return true;
     }
--- src/main/java/ru/job4j/tracker/SearchNameAction.java
+++ src/main/java/ru/job4j/tracker/SearchNameAction.java
@@ -1,21 +1,27 @@
 package ru.job4j.tracker;
 
 public class SearchNameAction implements UserAction {
+    private final Output out;
+
+    public SearchNameAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Find item for Name";
     }
 
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Find items by name ===");
+        out.println("=== Find items by name ===");
         String name = input.askStr("Enter name: ");
         Item[] items = tracker.findByName(name);
         if (items.length > 0) {
             for (Item item : items) {
-                System.out.println(item);
+                out.println(item);
             }
         } else {
-            System.out.println("Заявки с именем: " + name + " не найдены.");
+            out.println("Заявки с именем: " + name + " не найдены.");
         }
         return true;
     }
